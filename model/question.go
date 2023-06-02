0cc3c1b761fce657f817faa04326658a7633f05e
skay <skai-zhang@hotmail.com>
2023-06-02 11:52:55 +0800

    add question sql

--- model/question.go
+++ model/question.go
@@ -3,7 +3,7 @@ package model
 import "xorm.io/xorm"
 
 type Question struct {
-	ID       int     `json:"id"`
+	Id       int64   `json:"id"`
 	Subject  int     `json:"sid"`
 	Level    int     `json:"level"`
 	Type     int     `json:"type"`
@@ -17,15 +17,36 @@ type QuestionModel struct {
 	DB *xorm.Engine
 }
 
-func (model QuestionModel) GetQuestion(id int) (Question, error) {
-	query := "SELECT id, sid, level, type, question, options, answer, score FROM question WHERE id = ?"
-	var question Question
-	row, err := model.DB.Where(query, id).Get(&question)
+// 获取题目列表
+func (model QuestionModel) GetQuestionList(sid int, page int, num int) ([]Question, error) {
+	var questions []Question
+	err := model.DB.Where("sid = ?", sid).Limit(num, page).Find(&questions)
 	if err != nil {
-		return Question{}, err
+		return nil, err
 	}
-	if row {
-		return question, nil
+	return questions, nil
+}
+
+// 添加题目
+func (model QuestionModel) AddQuestion(question *Question) bool {
+	_, err := model.DB.Insert(question)
+	return err != nil
+}
+
+// 编辑题目
+func (model QuestionModel) EditQuestion(question *Question) bool {
+	if question.Id == 0 {
+		return false
 	}
-	return Question{}, nil
+	_, err := model.DB.ID(question.Id).Update(question)
+	return err != nil
+}
+
+// 删除题目
+func (model QuestionModel) DelQuestion(question *Question) bool {
+	if question.Id == 0 {
+		return false
+	}
+	_, err := model.DB.Delete(question)
+	return err != nil
 }
