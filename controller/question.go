f85989ac324da9cf48f832babccecec792ea8c24
Skay <skai-zhang@hotmail.com>
2023-06-04 01:38:27 +0800

    fix bug

--- controller/question.go
+++ controller/question.go
@@ -4,6 +4,7 @@ import (
 	"quest/global"
 	"quest/model"
 	"quest/service"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -19,10 +20,34 @@ type questionListResponse struct {
 }
 
 func (sc QuestionController) GetQuestionList(ctx *gin.Context) {
-	subs, err := sc.QuestionService.GetQuestionList()
-	if err != nil {
+	sid := ctx.Query("sid")
+	page := ctx.Query("page")
+	num := ctx.Query("number")
+	if len(sid) == 0 || len(page) == 0 {
+		global.ReturnError(ctx, global.Errors.ParamEmptyError)
+		return
+	}
+	iSid, err1 := strconv.Atoi(sid)
+	iPage, err2 := strconv.Atoi(page)
+	if err1 != nil || err2 != nil {
 		global.ReturnError(ctx, global.Errors.ParamIllegalError)
 		return
 	}
-	ctx.JSON(200, questionListResponse{List: subs, Time: time.Now().Unix()})
+	var err error
+	var list []model.Question
+	if len(num) == 0 {
+		list, err = sc.QuestionService.GetQuestionList(iSid, iPage, 10)
+	} else {
+		iNum, err := strconv.Atoi(page)
+		if err != nil {
+			global.ReturnError(ctx, global.Errors.ParamIllegalError)
+			return
+		}
+		list, err = sc.QuestionService.GetQuestionList(iSid, iPage, iNum)
+	}
+	if err != nil {
+		global.ReturnError(ctx, global.Errors.UnexpectedError)
+		return
+	}
+	ctx.JSON(200, questionListResponse{List: list, Time: time.Now().Unix()})
 }
