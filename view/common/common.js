c3417edb8e21cac0bd83437307d4eb1ab77084f9
Skay <skai-zhang@hotmail.com>
2023-06-05 02:44:57 +0800

    update index and auth page

--- view/common/common.js
+++ view/common/common.js
@@ -5,14 +5,13 @@
 function init() {
     $.get('/api/init', function (res) {
         if (res && res.name) {
+            $('.appName').text(res.name)
+            $('title').text($('title').text() + ' - ' + res.name)
+            $('body').append('<div class="absolute bottom-0 text-sm w-full text-center text-gray-400 pb-2">Powered by <a href="https://github.com/skye-z/quest" target="_blank">Quest</a> v' + res.version + '</div>')
+            $('#loading').hide();
             setTimeout(function () {
-                $('.appName').text(res.name)
-                $('body').append('<div class="absolute bottom-0 text-sm w-full text-center text-gray-400 pb-2">Powered by <a href="https://github.com/skye-z/quest" target="_blank">Quest</a> v' + res.version + '</div>')
-                $('#loading').hide();
-                setTimeout(function () {
-                    $('#loading').remove()
-                }, 300)
-            }, 500)
+                $('#loading').remove()
+            }, 300)
         } else {
             alert('应用初始化出错,请检查配置文件')
         }
