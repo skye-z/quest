a13082fd30287c051b71b3f44ec17bb4cfba0dd1
skay <skai-zhang@hotmail.com>
2023-06-07 15:46:19 +0800

    add theme config

--- page/src/App.vue
+++ page/src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <n-config-provider :theme-overrides="{ common: { fontWeightStrong: '600' } }">
+  <n-config-provider :theme="dark" :locale="i18n.main" :date-locale="i18n.date" :theme-overrides="theme">
     <router-view v-slot="{ Component }">
       <transition name="fade">
         <component :is="Component" />
       </transition>
     </router-view>
+    <NThemeEditor />
   </n-config-provider>
 </template>
 
 <script>
 import { init } from './plugins/api'
+import { darkTheme, zhCN, dateZhCN, NThemeEditor } from 'naive-ui'
+import theme from './theme.json'
 
 export default {
   name: "App",
+  components: { NThemeEditor },
   data: () => ({
+    dark: darkTheme,
+    theme,
+    i18n:{
+      main: zhCN,
+      date: dateZhCN
+    }
   }),
   methods: {
     checkLogin() {
@@ -22,8 +32,8 @@ export default {
     },
     initConfig() {
       init().then(res => {
-        localStorage.setItem('app:config:name',res.name)
-        localStorage.setItem('app:config:version',res.version)
+        localStorage.setItem('app:config:name', res.name)
+        localStorage.setItem('app:config:version', res.version)
         this.checkLogin()
       })
     }
